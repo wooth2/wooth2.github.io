361f5348c5b496ac39889cf8e54146e078c9643f
wooth2 <w01062400207@gmail.com>
2025-09-12 16:09:16 +0900

    Update 01_HelloWindow.js

--- 01_HelloWindow.js
+++ 01_HelloWindow.js
@@ -6,13 +6,22 @@ if (!gl) {
     console.error('WebGL 2 is not supported by your browser.');
 }
 
-// Set canvas size: 현재 window 전체를 canvas로 사용
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// Set canvas size: 처음 실행을 시작했을 때, canvas의 크기는 500 x 500 이어야 합니다.
+canvas.width = 500;
+canvas.height = 500;
 
 // Initialize WebGL settings: viewport and clear color
-gl.viewport(0, 0, canvas.width, canvas.height);
-gl.clearColor(0.1, 0.2, 0.3, 1.0);
+gl.viewport(0, 0, canvas.width/2, canvas.height/2);
+gl.clearColor(0, 0, 1, 1.0);
+
+gl.viewport(0, canvas.height/2, canvas.width/2, canvas.height/2);
+gl.clearColor(0, 1, 0, 1.0);
+
+gl.viewport(canvas.width/2, 0, canvas.width/2, canvas.height/2);
+gl.clearColor(1, 1, 0, 1.0);
+
+gl.viewport(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
+gl.clearColor(1, 0, 0, 1.0);
 
 // Start rendering
 render();
