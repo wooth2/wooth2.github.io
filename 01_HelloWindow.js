343467d61ce300e5dccaeb17cf655f8520acd1dc
wooth2 <w01062400207@gmail.com>
2025-09-12 16:15:50 +0900

    Update 01_HelloWindow.js

--- 01_HelloWindow.js
+++ 01_HelloWindow.js
@@ -28,6 +28,20 @@ render();
 
 // Render loop
 function render() {
+    gl.viewport(0, 0, canvas.width/2, canvas.height/2);
+    gl.clearColor(0, 0, 1, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);    
+
+    gl.viewport(0, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.clearColor(0, 1, 0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);    
+
+    gl.viewport(canvas.width/2, 0, canvas.width/2, canvas.height/2);
+    gl.clearColor(1, 1, 0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);    
+
+    gl.viewport(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.clearColor(1, 0, 0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);    
     // Draw something here
 }
