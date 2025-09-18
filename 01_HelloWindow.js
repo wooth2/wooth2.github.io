4fa927ff852de6142ade0e584e9e96a7d99b4273
wooth2 <w01062400207@gmail.com>
2025-09-18 18:24:42 +0900

    Update 01_HelloWindow.js

--- 01_HelloWindow.js
+++ 01_HelloWindow.js
@@ -11,36 +11,29 @@ canvas.width = 500;
 canvas.height = 500;
 
 // Initialize WebGL settings: viewport and clear color
-gl.viewport(0, 0, canvas.width/2, canvas.height/2);
-gl.clearColor(0, 0, 1, 1.0);
+gl.viewport(0, 0, canvas.width, canvas.height);
+gl.clearColor(0, 0, 0, 1.0);
 
-gl.viewport(0, canvas.height/2, canvas.width/2, canvas.height/2);
-gl.clearColor(0, 1, 0, 1.0);
-
-gl.viewport(canvas.width/2, 0, canvas.width/2, canvas.height/2);
-gl.clearColor(1, 1, 0, 1.0);
-
-gl.viewport(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
-gl.clearColor(1, 0, 0, 1.0);
+gl.enable(gl.SCISSOR_TEST); // render() 를 call하기 전 gl.enable(....); 로 무엇인가를 enable 시켜야 합니다.
 
 // Start rendering
 render();
 
 // Render loop
 function render() {
-    gl.viewport(0, 0, canvas.width/2, canvas.height/2);
+    gl.scissor(0, 0, canvas.width/2, canvas.height/2);
     gl.clearColor(0, 0, 1, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);    
 
-    gl.viewport(0, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.scissor(0, canvas.height/2, canvas.width/2, canvas.height/2);
     gl.clearColor(0, 1, 0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);    
 
-    gl.viewport(canvas.width/2, 0, canvas.width/2, canvas.height/2);
+    gl.scissor(canvas.width/2, 0, canvas.width/2, canvas.height/2);
     gl.clearColor(1, 1, 0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);    
 
-    gl.viewport(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.scissor(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
     gl.clearColor(1, 0, 0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);    
     // Draw something here
@@ -48,7 +41,7 @@ function render() {
 
 // Resize viewport when window size changes
 window.addEventListener('resize', () => {
-        // 창 크기 중 작은 면을 기준으로 정사각형 유지
+    // Window를 resize했을 때, canvas의 가로와 세로의 비율 1:1 유지 (canvas의 크기는 window size에 따라 변할 수 있습니다.)
     const size = Math.min(window.innerWidth, window.innerHeight);
     canvas.width = size;
     canvas.height = size;
